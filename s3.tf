82c6e7cdd9d08c161ac739870698e5d263ee1101
shrutika-2395 <shrutikashinde2395@gmail.com>
2024-10-23 14:29:53 +0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket = "my-unique-bucket-name"  # Ensure this name is globally unique
   tags = {
-    Name = "mydev-project-terraform-batch-25"
+    Name = "My S3 Bucket"
   }
 }
 
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_acl" "my-s3-bucket-acl" {
 resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
   bucket = aws_s3_bucket.my-s3-bucket.id
 
-  # Set the versioning status
-  status = "Enabled"  # Use "Enabled" or "Suspended"
+  versioning_configuration {
+    status = "Enabled"  # Use "Enabled" or "Suspended"
+  }
 }
-
