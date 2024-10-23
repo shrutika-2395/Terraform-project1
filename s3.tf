5d6fde00290863176181f498a2c1ac8ba11357fa
shrutika-2395 <shrutikashinde2395@gmail.com>
2024-10-23 14:11:14 +0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,15 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
+  bucket = "mydev-project-terraform-batch-25"  # Ensure this name is globally unique
+  acl    = "private"
+
+  tags = {
+    Name = "My S3 Bucket"
   }
-  
-  tags = var.tags
 }
+
+resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+
+  versioning_enabled = true
+}
+
