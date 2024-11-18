8c3cc0934df7161c03ca20ded8e6d5c3f1e225ee
shrutika-2395 <shrutikashinde2395@gmail.com>
2024-11-18 14:31:09 +0400

    Update s3.tf
    file changed

--- s3.tf
+++ s3.tf
@@ -1,19 +1,26 @@
-resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket = "my-unique-bucket-shrutika"  # Ensure this name is globally unique
-  tags = {
-    Name = "My S3 Bucket"
-  }
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-unique-bucket-shrutika"
 }
 
-resource "aws_s3_bucket_acl" "my-s3-bucket-acl" {
-  bucket = aws_s3_bucket.my-s3-bucket.id
-  acl    = "private"
+resource "aws_s3_bucket_object" "my_object" {
+  bucket = aws_s3_bucket.my_s3_bucket.bucket
+  key    = "example.txt"
+  source = "example.txt"
 }
 
-resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
-  bucket = aws_s3_bucket.my-s3-bucket.id
+resource "aws_s3_bucket_policy" "my_s3_bucket_policy" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  policy = data.aws_iam_policy_document.s3_bucket_policy.json
+}
 
-  versioning_configuration {
-    status = "Enabled"  # Use "Enabled" or "Suspended"
+data "aws_iam_policy_document" "s3_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.my_s3_bucket.arn}/*"]
+    effect    = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
   }
 }
